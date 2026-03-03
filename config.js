cbbbe8fe9da60b9c07d64a875c8c0743cce836d7
shplayer1 <trygungoo@gmail.com>
2026-03-04 01:26:07 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-window.__DASHBOARD_DATA_ENDPOINT__ = 'https://script.google.com/macros/s/AKfycbxsT_ibcYjGnsQu-BoqAwh9ZGwWbkziQFbcqSNTeiabhXL5J1UtQWXUx_y5zsi6XVk15Q/exec?action=dashboardData';
+window.__DASHBOARD_DATA_ENDPOINT__='https://script.google.com/macros/s/AKfycbyWE9l4tFaQirXHyUhQE2yKtTYs0DdFAWDthh67Loxx9sVRGNehgPMSr_6kMZDBJV5HFw/exec?action=dashboardData';
 window.__DASHBOARD_DATA_TOKEN__ = 'dashboard2026securetoken';
